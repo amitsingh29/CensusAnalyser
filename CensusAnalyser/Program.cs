a571d97a8c905a4d5464705954949ba78a5f1ae0
amitsingh29 <amitsinghbhakuni29@gmail.com>
2020-03-04 16:04:21 +0530

    [Amit] Add. First commit

---
+++ CensusAnalyser/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CensusAnalyser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("****************************************");
+            Console.WriteLine("    Welcome to Census Analyser          ");
+            Console.WriteLine("****************************************");
+        }
+    }
+}
