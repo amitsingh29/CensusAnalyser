74c03f69c3ad296ed616800313ac669f069f3241
amitsingh29 <amitsinghbhakuni29@gmail.com>
2020-03-05 11:36:03 +0530

    [Amit] Refactor. GivenTheStatesCensusCSVFile_CheckToEnsure_TheNumberOfRecordsMatches

--- Testing/UnitTest1.cs
+++ Testing/UnitTest1.cs
@@ -5,14 +5,12 @@ namespace Testing
 {
     public class Tests
     {
-        /*[Test]
+        [Test]
         public void GivenTheStatesCensusCSVFile_CheckToEnsureTheNumberOfRecordMatches()
         {
             StateCensusAnalyser stateCensus = new StateCensusAnalyser();
             string value = stateCensus.ReadData();
             Assert.AreEqual(value, "30");
         }
-        */
-        
     }
 }
