606211a6bc8a0906d576dec20044586bcd0a0ca0
amitsingh29 <amitsinghbhakuni29@gmail.com>
2020-03-05 16:20:11 +0530

    [Amit] Refactor. Adding GivenTheStateCensusCSVFile_IfIncorrect_ReturnsACustomException Test case

--- Testing/UnitTest1.cs
+++ Testing/UnitTest1.cs
@@ -13,13 +13,13 @@ namespace Testing
             Assert.AreEqual(value, 30 );
         }
 
-        /*[Test]
+        [Test]
         public void GivenTheStateCensusCSVFile_IfIncorrect_ReturnsACustomException()
         {
             StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
             var value = Assert.Throws<CustomException>(()=>stateCensusAnalyser.ReadData(@"C:\Users\ye10397\Desktop\Amit\StateCensus.csv"));
             Assert.AreEqual("file not found", value.GetMessage);
-        }*/
+        }
 
 
         
