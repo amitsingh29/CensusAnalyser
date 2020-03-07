0230814c9741d0d4301a98d47ee40ffc3a85868d
amitsingh29 <amitsinghbhakuni29@gmail.com>
2020-03-07 15:32:18 +0530

    [Amit] Refactor. StateCensusAnalyser

--- CensusAnalyser/CSVStateCensus.cs
+++ CensusAnalyser/CSVStateCensus.cs
@@ -1,38 +1,84 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.IO;
+﻿//-----------------------------------------------------------------------
+// <copyright file="CSVStateCensus.cs" company="BridgeLabz">
+//     Copyright © 2020
+// </copyright>
+// <creator name="Amit Singh"/>
+//-----------------------------------------------------------------------
 
 namespace CensusAnalyser
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.IO;
+
+    /// <summary>
+    /// CSVStateCensus class
+    /// </summary>
     public class CSVStateCensus
     {
+        /// <summary>
+        /// The Path
+        /// </summary>
         private string filePath;
+
+        /// <summary>
+        /// The Delimiter
+        /// </summary>
         private char delimiter = ',';
+
+        /// <summary>
+        /// The Header
+        /// </summary>
         private string header = "State,Population,AreaInSqKm,DensityPerSqKm";
+
+        /// <summary>
+        /// Count the numberOfRecords
+        /// </summary>
         int numberOfRecords = 0;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CSVStateCensus"/> class.
+        /// </summary>
         public CSVStateCensus()
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CSVStateCensus"/> class.
+        /// </summary>
+        /// <param name="filePath"></param>
         public CSVStateCensus(string filePath)
         {
             this.filePath = filePath;
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CSVStateCensus"/> class.
+        /// </summary>
+        /// <param name="filePath">The path</param>
+        /// <param name="delimiter">The delimiter</param>
         public CSVStateCensus(string filePath, char delimiter)
         {
             this.filePath = filePath;
             this.delimiter = delimiter;
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CSVStateCensus"/> class.
+        /// </summary>
+        /// <param name="filePath">The path</param>
+        /// <param name="header">The header</param>
         public CSVStateCensus(string filePath, string header)
         {
             this.filePath = filePath;
             this.header = header;
         }
 
-
+        /// <summary>
+        /// Number Of Records
+        /// </summary>
+        /// <returns>Returns the number of lines</returns>
         public string ReadData()
         {
             try
@@ -75,6 +121,5 @@ namespace CensusAnalyser
                 return ex.Message;
             }
         }
-
     }
 }
