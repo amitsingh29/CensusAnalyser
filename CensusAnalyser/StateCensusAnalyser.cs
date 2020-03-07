86e3dbb9641f38fff1c68000f743ccde3d2df958
amitsingh29 <amitsinghbhakuni29@gmail.com>
2020-03-07 15:40:53 +0530

    [Amit] Refactor. Adding Stylecop in StateCensusAnalyser

--- CensusAnalyser/StateCensusAnalyser.cs
+++ CensusAnalyser/StateCensusAnalyser.cs
@@ -1,22 +1,47 @@
-﻿using System;
-using System.IO;
-using System.Collections.Generic;
-using System.Linq;
+﻿//-----------------------------------------------------------------------
+// <copyright file="StateCensusAnalyser.cs" company="BridgeLabz">
+//     Copyright © 2020
+// </copyright>
+// <creator name="Amit Singh"/>
+//-----------------------------------------------------------------------
 
 namespace CensusAnalyser
-{ }
+{
+    using System;
+    using System.IO;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    /// <summary>
+    /// StateCensusAnalyser class
+    /// </summary>
     public class StateCensusAnalyser
     {
+        /// <summary>
+        /// File Path
+        /// </summary>
         private string filePath;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StateCensusAnalyser"/> class.
+        /// </summary>
         public StateCensusAnalyser()
         {
         }
-        
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StateCensusAnalyser"/> class.
+        /// </summary>
+        /// <param name="filePath">The Path</param>
         public StateCensusAnalyser(string filePath)
         {
             this.filePath = filePath;
         }
 
+        /// <summary>
+        /// Number Of Records
+        /// </summary>
+        /// <returns>The Number Of Lines</returns>
         public string ReadFileData()
         {
             string[] lines = File.ReadAllLines(filePath);
