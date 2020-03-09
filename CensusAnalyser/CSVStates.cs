96bcc34023b5712171af9e4a43809cac0510c4ea
amitsingh29 <amitsinghbhakuni29@gmail.com>
2020-03-09 11:00:45 +0530

    [Amit] Add. CSVStates

---
+++ CensusAnalyser/CSVStates.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CensusAnalyser
+{
+    public class CSVStates
+    {
+        int numberOfLines = 0;
+        public int getData(string path)
+        {
+            string[] str = File.ReadAllLines(path);
+            IEnumerable<string> getLines = str;
+            foreach(var lines in getLines)
+            {
+                numberOfLines++;
+            }
+            return numberOfLines;
+        }
+    }
+}
--- CensusAnalyser/Program.cs
+++ CensusAnalyser/Program.cs
@@ -5,10 +5,10 @@ namespace CensusAnalyser
     class Program
     {
         static void Main(string[] args)
-        {
-            CSVStateCensus cSVStateCensus = new CSVStateCensus(@"C:\Users\ye10397\Desktop\Amit\StateCensusData.csv");
-            string data = cSVStateCensus.ReadData();
-            Console.WriteLine(data);
+        {/*
+            CSVStates cSV = new CSVStates();
+            int record = cSV.getData(@"C:\Users\ye10397\Desktop\Amit\StateCode.csv");
+            Console.WriteLine("The number of lines are: " + record);*/
         }
     }
 }
