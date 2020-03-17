cfcbfb6fa8d92c5f379c26a222b26bc6b0fdefe3
amitsingh29 <amitsinghbhakuni29@gmail.com>
2020-03-17 12:21:15 +0530

    [Amit] Refactor. added Stylecop in CSVFactory

--- CensusAnalyser/CSVFactory.cs
+++ CensusAnalyser/CSVFactory.cs
@@ -1,27 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//-----------------------------------------------------------------------
+// <copyright file="CSVFactory.cs" company="BridgeLabz">
+//     Copyright © 2020
+// </copyright>
+// <creator name="Amit Singh"/>
+//-----------------------------------------------------------------------
 
 namespace CensusAnalyser
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// CSVFactory class
+    /// </summary>
     public class CSVFactory 
     {
         /// <summary>
         /// Interface ICSVBuilder
         /// </summary>
-        /// <param name="className"></param>
-        /// <returns>className</returns>
+        /// <param name="className">The className</param>
+        /// <returns>starting className or ending className</returns>
         public static ICSVBuilder Factory(string className)
         {
-            switch(className)
+            switch (className)
             {
                 case "CSVStates":
                     return new CSVStates();
                 case "CSVStateCensus":
                     return new CSVStateCensus();
             }
+
             return null;
         }
     }
 }
-
